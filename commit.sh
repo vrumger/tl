def4cd6033fa1d9253677a34245c7b7ee9f885a1
Andrew Lane <contact@lungers.com>
2021-10-09 20:30:15 +0300

    Add the layer number to the commit message

--- commit.sh
+++ commit.sh
@@ -2,6 +2,6 @@ if [ ! -z "$(git status --porcelain schemes/)" ]; then
     git config user.name "Lungers Bot"
     git config user.email "github-bot@lungers.com"
     git add schemes/
-    git commit -m "Update TL scheme"
+    git commit -m "Update to layer $(grep -oP '(?<=^// LAYER )([0-9]+)$' schemes/latest.tl || echo unknown)"
     git push
 fi
