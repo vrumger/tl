9e384a0353873f92474f3e0e2b35f3ac11df9f93
Avrumy <contact@lungers.com>
2023-11-29 18:24:09 +0200

    Add last-commit.txt when committing new schemes

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 if [ ! -z "$(git status --porcelain schemes/)" ]; then
     git config user.name "Lungers Bot"
     git config user.email "github-bot@lungers.com"
-    git add schemes/
+    git add last-commit.txt schemes/
     git commit -m "Update to layer $(grep -oP '(?<=^// LAYER )([0-9]+)$' schemes/latest.tl || echo unknown)"
     git push
 fi
