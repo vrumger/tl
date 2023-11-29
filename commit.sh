1196536197575f657f69bbbe26f4062715ff1c4b
Avrumy <contact@lungers.com>
2023-11-29 18:24:27 +0200

    Fix parsing the layer for new scheme commits

--- commit.sh
+++ commit.sh
@@ -2,6 +2,6 @@ if [ ! -z "$(git status --porcelain schemes/)" ]; then
     git config user.name "Lungers Bot"
     git config user.email "github-bot@lungers.com"
     git add last-commit.txt schemes/
-    git commit -m "Update to layer $(grep -oP '(?<=^// LAYER )([0-9]+)$' schemes/latest.tl || echo unknown)"
+    git commit -m "Update to layer $(jq -r .layer schemes/layer.json)"
     git push
 fi
